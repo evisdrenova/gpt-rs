fde35142396fbc696c231976df47493649f2c4b3
Evis Drenova <evis@usenucleus.cloud>
2025-10-16 22:07:50 -0700

    feat(main.py): update dataloader and embedding layers

--- py-gpt/main.py
+++ py-gpt/main.py
@@ -46,18 +46,28 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=
 
 
 input_ids = torch.tensor([2, 3, 5, 1])
-vocab_size = 6
-output_dim = 3
+vocab_size = 50257
+output_dim = 256
+max_length  = 4
 
-torch.manual_seed(123)
-embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
-print(embedding_layer(input_ids))
-print(embedding_layer(torch.tensor(2)))
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=max_length,stride=max_length, shuffle=False)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Token IDs:\n", inputs)
+print("\nInputs shape:\n", inputs.shape)
 
+token_embeddings = token_embedding_layer(inputs)
+print("token embeddings shape:",token_embeddings.shape)
 
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+print("positional embedding shape:",pos_embeddings.shape)
 
-
-
+# absolute positional encoding
+input_embeddings = token_embeddings + pos_embeddings
+print("input embeddings shape:",input_embeddings.shape)
 
 # if __name__ == "__main__":
   
