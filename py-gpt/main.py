5eaa54c661d8b67f9215a64b15a181e01f0e1418
Evis Drenova <evis@usenucleus.cloud>
2025-10-17 15:53:06 -0700

    feat: add self attention class and update main.py with tensor operations

--- py-gpt/main.py
+++ py-gpt/main.py
@@ -54,20 +54,54 @@ token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
 dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=max_length,stride=max_length, shuffle=False)
 data_iter = iter(dataloader)
 inputs, targets = next(data_iter)
-print("Token IDs:\n", inputs)
-print("\nInputs shape:\n", inputs.shape)
 
 token_embeddings = token_embedding_layer(inputs)
-print("token embeddings shape:",token_embeddings.shape)
 
 context_length = max_length
 pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
 pos_embeddings = pos_embedding_layer(torch.arange(context_length))
-print("positional embedding shape:",pos_embeddings.shape)
 
 # absolute positional encoding
 input_embeddings = token_embeddings + pos_embeddings
-print("input embeddings shape:",input_embeddings.shape)
+
+inputs = torch.tensor(
+[[0.43, 0.15, 0.89], # Your (x^1)
+[0.55, 0.87, 0.66], # journey (x^2)
+[0.57, 0.85, 0.64], # starts (x^3)
+[0.22, 0.58, 0.33], # with (x^4)
+[0.77, 0.25, 0.10], # one (x^5)
+[0.05, 0.80, 0.55]] # step (x^6)
+)
+
+x_2 = inputs[1]
+d_in = inputs.shape[1]
+d_out = 2
+
+torch.manual_seed(123)
+W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+
+query_2 = x_2 @ W_query
+key_2 = x_2 @ W_key
+value_2 = x_2 @ W_value
+print(query_2)
+
+keys = inputs @ W_key
+values = inputs @ W_value
+print("keys.shape:", keys.shape)
+print("values.shape:", values.shape)
+
+attn_scores_2 = query_2 @ keys.T
+print(attn_scores_2)
+
+d_k = keys.shape[-1]
+attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1)
+print(attn_weights_2)
+
+context_vec_2 = attn_weights_2 @ values
+print(context_vec_2)
+
 
 # if __name__ == "__main__":
   
