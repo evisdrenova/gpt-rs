7f304a48f342a8c55633a0d8c624fd6bedf80e21
Evis Drenova <evis@usenucleus.cloud>
2025-10-17 22:59:47 -0700

    feat: refactor SelfAttention class and remove unused code in main.py

--- py-gpt/attention.py
+++ py-gpt/attention.py
@@ -1,21 +1,30 @@
-import torch.nn as nn
+
+from multiprocessing import context
 import torch
 
-class SelfAttention_v1(nn.Module):
-    def __init__(self, d_in, d_out):
+class SelfAttention(torch.nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
         super().__init__()
-        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
-        
-        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.d_out = d_out
+        self.W_query = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = torch.nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = torch.nn.Dropout(dropout)
+        self.register_buffer('mask',torch.triu(torch.ones(context_length, context_length),diagonal=1))
 
-        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
 
-        def forward(self, x):
-            keys = x @ self.W_key
-            queries = x @ self.W_query
-            values = x @ self.W_value
 
-            attn_scores = queries @ keys.T # omega dot product
-            attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
-            context_vec = attn_weights @ values
-            return context_vec
+    def forward(self, x):
+        b, num_tokens,d_in = x.shape
+
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1,2)
+        attn_scores.masked_fill_(self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+        context_vec = attn_weights @ values
+
+        return context_vec
--- py-gpt/main.py
+++ py-gpt/main.py
@@ -1,8 +1,7 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 import tiktoken
-import time
-import re
+from attention import SelfAttention
 # from model import GPTModel, generate_text_simple,create_dataloader_v1, calc_loss_loader,train_model_simple
 
 with open('../the-verdict.txt',"r", encoding="utf-8") as f:
@@ -45,25 +44,6 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=
 
 
 
-input_ids = torch.tensor([2, 3, 5, 1])
-vocab_size = 50257
-output_dim = 256
-max_length  = 4
-
-token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
-dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=max_length,stride=max_length, shuffle=False)
-data_iter = iter(dataloader)
-inputs, targets = next(data_iter)
-
-token_embeddings = token_embedding_layer(inputs)
-
-context_length = max_length
-pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
-pos_embeddings = pos_embedding_layer(torch.arange(context_length))
-
-# absolute positional encoding
-input_embeddings = token_embeddings + pos_embeddings
-
 inputs = torch.tensor(
 [[0.43, 0.15, 0.89], # Your (x^1)
 [0.55, 0.87, 0.66], # journey (x^2)
@@ -73,35 +53,15 @@ inputs = torch.tensor(
 [0.05, 0.80, 0.55]] # step (x^6)
 )
 
-x_2 = inputs[1]
-d_in = inputs.shape[1]
-d_out = 2
+batch = torch.stack((inputs, inputs), dim=0)
+print(batch.shape)
+
 
 torch.manual_seed(123)
-W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-W_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
-
-query_2 = x_2 @ W_query
-key_2 = x_2 @ W_key
-value_2 = x_2 @ W_value
-print(query_2)
-
-keys = inputs @ W_key
-values = inputs @ W_value
-print("keys.shape:", keys.shape)
-print("values.shape:", values.shape)
-
-attn_scores_2 = query_2 @ keys.T
-print(attn_scores_2)
-
-d_k = keys.shape[-1]
-attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1)
-print(attn_weights_2)
-
-context_vec_2 = attn_weights_2 @ values
-print(context_vec_2)
-
+context_length = batch.shape[1]
+ca = SelfAttention(d_in, d_out, context_length, 0.0)
+context_vecs = ca(batch)
+print("context_vecs.shape:", context_vecs.shape)
 
 # if __name__ == "__main__":
   
