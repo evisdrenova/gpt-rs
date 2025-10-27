2248acf3fe15c09a8ef41c801a7835cd6bf65eb8
Evis Drenova <evis@usenucleus.cloud>
2025-10-26 21:16:39 -0700

    feat: add simple training function and integrate into main.py

--- py-gpt/main.py
+++ py-gpt/main.py
@@ -7,6 +7,7 @@ from activations import GELU, FeedForward
 from generation import token_ids_to_text, text_to_token_ids, generate_text_simple
 from dataset import create_dataloader_v1
 from loss import calc_loss_loader
+from train import train_model_simple
 
 GPT_CONFIG_124M = {
 "vocab_size": 50257,
@@ -54,21 +55,20 @@ shuffle=False,
 num_workers=0
 )
 
-print("Train loader:")
-for x, y in train_loader:
-    print(x.shape, y.shape)
-print("\nValidation loader:")
-for x, y in val_loader:
-    print(x.shape, y.shape)
+
 
 torch.manual_seed(123)
 model = GPTModel(GPT_CONFIG_124M)
-model.eval()
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-with torch.no_grad():
-    train_loss = calc_loss_loader(train_loader, model, device)
-    val_loss = calc_loss_loader(val_loader, model, device)
-print("Training loss:", train_loss)
-print("Validation loss:", val_loss)
+optimizer = torch.optim.AdamW(
+model.parameters(),
+lr=0.0004, weight_decay=0.1
+)
+num_epochs = 10
+train_losses, val_losses, tokens_seen = train_model_simple(
+model, train_loader, val_loader, optimizer, device,
+num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+start_context="Every effort moves you", tokenizer=tokenizer
+)
+
