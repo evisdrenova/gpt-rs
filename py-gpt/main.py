6b2eb83e1d090242807c7ac68a8fca08f8c5a7e1
Evis Drenova <evis@usenucleus.cloud>
2025-10-27 22:18:25 -0700

    feat: add generate function and update main.py for text generation

--- py-gpt/main.py
+++ py-gpt/main.py
@@ -4,7 +4,7 @@ import tiktoken
 from attention import MultiHeadAttention, SelfAttention
 from model import  GPTModel,LayerNorm, TransformerBlock
 from activations import GELU, FeedForward
-from generation import token_ids_to_text, text_to_token_ids, generate_text_simple
+from generation import generate, token_ids_to_text, text_to_token_ids, generate_text_simple
 from dataset import create_dataloader_v1
 from loss import calc_loss_loader
 from train import train_model_simple
@@ -54,6 +54,7 @@ num_workers=0
 
 
 
+
 torch.manual_seed(123)
 model = GPTModel(GPT_CONFIG_124M)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -62,30 +63,42 @@ optimizer = torch.optim.AdamW(
 model.parameters(),
 lr=0.0004, weight_decay=0.1
 )
-num_epochs = 10
-train_losses, val_losses, tokens_seen = train_model_simple(
-model, train_loader, val_loader, optimizer, device,
-num_epochs=num_epochs, eval_freq=5, eval_iter=5,
-start_context="Every effort moves you", tokenizer=tokenizer
+# num_epochs = 10
+# train_losses, val_losses, tokens_seen = train_model_simple(
+# model, train_loader, val_loader, optimizer, device,
+# num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+# start_context="Every effort moves you", tokenizer=tokenizer
+# )
+
+torch.manual_seed(123)
+token_ids = generate(
+model=model,
+idx=text_to_token_ids("Every effort moves you", tokenizer),
+max_new_tokens=15,
+context_size=GPT_CONFIG_124M["context_length"],
+top_k=25,
+temperature=1.4
 )
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
 
 
 
-def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
-    fig, ax1 = plt.subplots(figsize=(5, 3))
-    ax1.plot(epochs_seen, train_losses, label="Training loss")
-    ax1.plot(
-    epochs_seen, val_losses, linestyle="-.", label="Validation loss"
-    )
-    ax1.set_xlabel("Epochs")
-    ax1.set_ylabel("Loss")
-    ax1.legend(loc="upper right")
-    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
-    ax2 = ax1.twiny()
-    ax2.plot(tokens_seen, train_losses, alpha=0)
-    ax2.set_xlabel("Tokens seen")
-    fig.tight_layout()
-    plt.show()
-epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
-plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+
+# def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+#     fig, ax1 = plt.subplots(figsize=(5, 3))
+#     ax1.plot(epochs_seen, train_losses, label="Training loss")
+#     ax1.plot(
+#     epochs_seen, val_losses, linestyle="-.", label="Validation loss"
+#     )
+#     ax1.set_xlabel("Epochs")
+#     ax1.set_ylabel("Loss")
+#     ax1.legend(loc="upper right")
+#     ax1.xaxis.set_major_locator(MaxNLocator(integer=True))
+#     ax2 = ax1.twiny()
+#     ax2.plot(tokens_seen, train_losses, alpha=0)
+#     ax2.set_xlabel("Tokens seen")
+#     fig.tight_layout()
+#     plt.show()
+# epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+# plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
 
